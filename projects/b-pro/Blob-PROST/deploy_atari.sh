647392c71504d0660032765f490d76e25390432e
Suraj Narayanan S <surajx@gmail.com>
2016-10-05 23:11:35 +1100

    deploy changes

--- projects/b-pro/Blob-PROST/deploy_atari.sh
+++ projects/b-pro/Blob-PROST/deploy_atari.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 game_array=( venture montezuma_revenge freeway ) #frostbite qbert gravitar freeway bank_heist alien )
-beta_array=( 0.00001 0.0001 0.00025 0.0005 0.00075 0.001 ) # 
+beta_array=( 0.00001 0.0001 0.00025 ) # 0.0005 0.00075 0.001 )
 dt=`date +%H%M`
 loc=$1
+romLoc=$2
 
 for j in "${beta_array[@]}";
 do
@@ -12,6 +13,6 @@ do
     for i in "${game_array[@]}";
     do
         seed=$(( ( RANDOM % 10000 )  + 1 ))
-        ./learnerBlobTime -s $seed -c ../conf/bpro.cfg -r /home/users/u5881495/git/directed-explore/projects/Arcade-Learning-Environment/ROMS/$i.bin -w "$i"_phi_wts_"$loc"_beta$j -n "$i"_phi_wts_checkpoint_"$loc"_beta$j | gzip -9 > "$i"_phi_wts_"$loc"_beta"$j"_051016_"$dt".log.gzip &
+        ./learnerBlobTime -s $seed -c ../conf/bpro.cfg -r $romLoc/$i.bin -w "$i"_phi_wts_"$loc"_beta$j -n "$i"_phi_wts_checkpoint_"$loc"_beta$j | xz -9 > "$i"_phi_wts_"$loc"_beta"$j"_051016_"$dt".log.xz &
     done
 done
