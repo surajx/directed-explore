167e06c47beeeea878098d6ebea8f51a908e7eff
Suraj Narayanan S <suraj.sasikumar@anu.edu.au>
2016-12-12 11:01:53 +1100

    update deploy

--- projects/phi_explore/deploy_phi_explore.sh
+++ projects/phi_explore/deploy_phi_explore.sh
@@ -1,16 +1,56 @@
 #!/bin/bash
-if [ "$#" -ne 3 ]; then
-    echo "Illegal number of parameters"
+
+# Configure beta values, and games to be run here
+#################################################
+game_array=( montezuma_revenge freeway ) #venture frostbite qbert gravitar bank_heist alien )
+beta_array=( 0.005 0.01 0.015 0.02 0.025 0.03 0.035 0.04 0.045 0.05 0.06 0.07 )
+#################################################
+
+# Help
+if [ "$#" -ne 6 ]; then
     echo "Usage:"
-    echo "./deploy_phi_explore.sh <system_name> <beta_block> <trials_per_game>"
+    echo "./deploy_phi_explore.sh <system_name> <beta_block> <trials_per_game> <system_setup=true|false> <build_ale=true|false> <build_agent=true|false>"
+    
+    num_cores=`lscpu | grep -i Core | grep -i "per socket" | awk '{ print $4}'`
+    len=$(($num_cores / ${#game_array[@]}))
+    num_beta_blks=$((${#beta_array[@]} / $len))
+    for i in `seq 1 $num_beta_blks`;
+    do
+        start=$(($i * $len - $len))
+        beta_block=("${beta_array[@]:$start:$len}")    
+        echo "Beta Block "$i":"${beta_block[@]}
+    done
     exit
+fi
+
+# Install required libraries
+if [ "$4" == "true" ]; then
+    sudo apt-get update
+    sudo apt-get upgrade
+    sudo apt-get install libsdl1.2-dev libsdl-gfx1.2-dev libsdl-image1.2-dev cmake # libstdc++6
+fi
+
+# Build ALE
+if [ "$5" == "true" ]; then
+    cd ./ALE
+    num_proc=`nproc`
+    make clean -f makefile.unix
+    make all -f makefile.unix -j$num_proc
+    cd ../
 fi
 
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:"`pwd`"/ALE"
 
-game_array=( montezuma_revenge freeway ) #venture frostbite qbert gravitar bank_heist alien )
-beta_array=( 0.005 0.01 0.015 0.02 0.025 0.03 0.035 0.04 0.045 0.05 0.06 0.07 )
+# Build agent
+if [ "$6" == "true" ]; then
+    cd ./agent/Blob-PROST
+    num_proc=`nproc`
+    make clean
+    make all -j$num_proc
+    cd ../../
+fi
 
+# Run agent
 agent_conf="./agent/conf/bpro.cfg"
 
 dt=`date +%H%M%S`
@@ -18,10 +58,9 @@ sysname=$1
 num_cores=`lscpu | grep -i Core | grep -i "per socket" | awk '{ print $4}'`
 romLoc="./ALE/ROMS"
 
-eff_coref=$(($num_cores / ${#game_array[@]}))
-end=$(($2 * $eff_coref))
-start=$(($end - $eff_coref))
-beta_block=("${beta_array[@]:$start:$end}")
+len=$(($num_cores / ${#game_array[@]}))
+start=$(($2 * $len - $len))
+beta_block=("${beta_array[@]:$start:$len}")
 
 repeat_game=$3
 
