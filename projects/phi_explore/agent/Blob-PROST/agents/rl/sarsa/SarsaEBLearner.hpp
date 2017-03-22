67194fd64d7d99beb01e932d1d3f1f84523a6dbf
Suraj Narayanan <surajx@gmail.com>
2017-03-23 01:16:24 +1100

    Added author
    Added author and TODO

--- projects/phi_explore/agent/Blob-PROST/agents/rl/sarsa/SarsaEBLearner.hpp
+++ projects/phi_explore/agent/Blob-PROST/agents/rl/sarsa/SarsaEBLearner.hpp
@@ -5,7 +5,7 @@
  **  Some updates are made to make it more efficient, as not iterating over all
  **  features.
  **
- ** Author:
+ ** Author: Suraj Narayanan Sasikumar
  ******************************************************************************/
 
 #ifndef SARSAEBLEARNER_H
@@ -43,67 +43,7 @@ class SarsaEBLearner : public SarsaLearner {
    */
   SarsaEBLearner();
 
-  /**
-  * The distance between current state-action pair and a centroid in feature
-  * space.
-  * @param vector<long long>& sa_cur a concatenation of the feature vector
-  *        actions, obtain simulator screen, RAM, etc.
-  * @param vector<long long>& centroid One centroid obtained from a clustering
-  *        algorithm. If the number of visited state-action pairs is small,
-  *        the centroids may simply be positions of the visited state-action
-  *        pairs in feature space.
-  */
-  // void metric(vector<long long>& sa_cur, vector<long long>& centroid);
-
-  /**
-  * Measure of similarity between state-action pairs and a centroid.
-  * Computed using a gaussian kernel centered on the centroid. The Measure
-  * takes values between 0 and 1.
-  * @param vector<long long>& sa_cur A concatenation of the feature vector
-  *        and action.
-  * @param vector<long long>& centroid One centroid obtained from a clustering
-  *        algorithm. If the number of visited state-action pairs is small,
-  *        the centroids may simply be positions of the visited state-action
-  *        pairs in feature space.
-  */
-  // void similarity_measure(vector<long long>& sa_cur,
-  //                         vector<long long>& centroid);
-
-  /**
-  * A generalized state-action visit count. Roughly represents the visit
-  * density at a point in feature space.
-  * @param vector<long long>& sa_cur A concatenation of the feature vector
-  *        and action.
-  */
-  // double pseudo_count(vector<long long>& sa_cur);
-
-  /**
-  * A bonus added to the external reward to incentivise exploration. Roughly
-  * a local information reward for visiting a state-action pair.
-  * @param vector<long long>& features The feature used in the linear approx.
-  * of the Q-function.
-  * @param long int: time steps elapsed
-  */
-  // double exploration_bonus(vector<long long>& features, long time_step);
-
-  /**
-  * Update the running probablity measure for occurance for each feature.
-  * Update Formula: mu_{t+1} = mu_t + (phi_{t+1} - mu_t)/(1+t)
-  * @param vector<long long>& features The feature used in the linear approx.
-  * of the Q-function.
-  * @param int no of time steps elapsed.
-  */
-  // void update_prob_feature(vector<long long>& features, long time_step);
-
-  /**
-  * If the feature has not been seen before we create a new map entry of the
-  *   form [feature index] : vector {
-  *              p(phi_{i}),
-  *              seen_flag, # 1-seen, 0-not
-  *              p(a_1/phi_{i}),...,p(a_n/phi_{i}),
-  *              n_phi # No of time phi has been active
-  *   }
-  */
+  //TODO: Documentation
   void add_new_feature_to_map(long long featIdx, int time_step);
 
   void update_phi(vector<long long>& features, long time_step);
